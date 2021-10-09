d622e4687ebb0b86d74d94ca5a2f71a932fb83c3
DXVII <david.pham.d@gmail.com>
2021-10-09 18:48:43 +1100

    Feature Select good 2 go

--- AutomationPipeline/FeatureSelect/MLEvaluation.py
+++ AutomationPipeline/FeatureSelect/MLEvaluation.py
@@ -12,47 +12,54 @@ infile = open("avgStatsPickle",'rb')
 statsDict = pickle.load(infile)
 print("stats loaded")
 
+
+
 # =============================================================================
 # === Separate Mean and Std Dev per State per Plot ============================
 # =============================================================================
 
+l_stateLabels = []
+list_Acc = []
 # for each key in statsDict
 for state in statsDict:
     
        # label for title
        stateLabel = state.split("CSVData")[0]
+       l_stateLabels.append(stateLabel)
 
        # collect value of each
        strLabels = []
-
        list_Prec = []
        list_Recall = []
        list_F1 = []
        list_Sup = []
+       
+       list_Acc.append(statsDict[state]['accuracy'])
        for intents in statsDict[state]:
+              
               try:
                      strLabels.append(ALLSTATES_DIC[int(intents)])
               except ValueError:
                      continue
-
+              
               scoresDict = (statsDict[state])[intents]
                      
-                     # Precision
+              # Precision
               list_Prec.append(scoresDict['precision'])
                      
-                     # Recall
+              # Recall
               list_Recall.append(scoresDict['recall'])
                      
-                     # F1 score
+              # F1 score
               list_F1.append(scoresDict['f1-score'])
                      
-                     # Support
+              # Support
               list_Sup.append(scoresDict['support'])
+
 # =============================================================================
 # === Plotting bar charts per State ===========================================
 # =============================================================================
        x_pos = np.arange(len(strLabels))
-
 
 
        # Plot bar graph Precision
@@ -119,6 +126,23 @@ for state in statsDict:
        ax.yaxis.grid(True)
        plt.tight_layout()
        plt.show()
+
+
+# Plot bar graph Accuracy
+x_pos = np.arange(len(l_stateLabels))
+fig, ax = plt.subplots()
+ax.bar(x_pos, list_Acc,
+       align='center',
+       alpha=0.5,
+       ecolor='black',
+       capsize=10)
+ax.set_ylabel('Accuracy values')
+ax.set_xticks(x_pos)
+ax.set_xticklabels(l_stateLabels)
+ax.set_title('Accuracy Plot for all Stationary States')
+ax.yaxis.grid(True)
+plt.tight_layout()
+plt.show()
 
 # # https://problemsolvingwithpython.com/06-Plotting-with-Matplotlib/06
 
